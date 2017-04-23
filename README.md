8de71de2fb7bd2b48b31fdbd2a7539a38ee77d31
Valentin Deville <contact@valentin-deville.eu>
2017-04-23 20:32:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pictures organizer bash script
+
+This script check for JPG files in folder and sort all raw and jpeg files in different folders sort by reference jpg files.
+
+
+### Usage
+
+1:
+Sort your JPG files:
+![firststep](/images/before.jpg)
+
+2:
+Call script:
+```
+bash script.sh /path/to/pictures/
+```
+
+3:
+It's done !
+![thirdstep](/images/after.jpg)
+
---
+++ script.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Colors:
+COLORRESET="\033[0m"
+COLORBOLD="\033[1m"
+COLORLIGHTRED="\033[91m"
+COLORLIGHYELLOW="\033[93m"
+COLORLIGHGREEN="\033[92m"
+COLORLIGHCYAN="\033[96m"
+
+# Check the path if properly given
+if [ $# -eq 0 ]
+  then
+    echo "${COLORLIGHCYAN}============================================${COLORRESET}"
+    echo " ${COLORLIGHYELLOW}Please specify the folder path of pictures ${COLORRESET}"
+    echo "${COLORLIGHCYAN}============================================${COLORRESET}"
+    echo ""
+    echo "Ex: ./script.sh /path/to/pictures/"
+    echo ""
+    exit
+fi
+
+
+echo "${COLORLIGHYELLOW}Are sure you had sort all .JPG files ? (${COLORLIGHGREEN}yes${COLORRESET} ${COLORLIGHYELLOW}/ ${COLORLIGHTRED}no${COLORLIGHYELLOW})${COLORRESET}"
+read areyousure
+
+if [ ${areyousure} != "yes" ]
+then
+    exit 0
+fi
+
+BASEDIR=${1}
+
+JPGFILES=${BASEDIR}/jpg/
+RAWFILES=${BASEDIR}/raw/
+JUNKFILES=${BASEDIR}/junk/
+
+KEEPCOUNTDOWN=0
+JUNKCOUNTDOWN=0
+
+mkdir -p ${JPGFILES}
+mkdir -p ${RAWFILES}
+mkdir -p ${JUNKFILES}
+
+# For each pictures in folder
+for file in ${BASEDIR}*
+do
+  file=$(basename ${file})
+  extension="${file##*.}"
+  filename="${file%.*}"
+
+  # Check JPG files
+  if [ "$extension" == "JPG" ] ; then
+      mv ${BASEDIR}${filename}.JPG ${JPGFILES}
+      mv ${BASEDIR}${filename}.CR2 ${RAWFILES}
+      ((KEEPCOUNTDOWN++))
+  fi
+done
+
+# Junk all residual files
+for file in ${BASEDIR}*
+do
+  if [ -f ${file} ] ; then
+    mv ${file} ${JUNKFILES}
+    ((JUNKCOUNTDOWN++))
+  fi
+done
+
+echo "${COLORLIGHGREEN}${COLORBOLD}${KEEPCOUNTDOWN}${COLORRESET}${COLORLIGHGREEN} saved pictures ${COLORRESET}"
+echo "${COLORLIGHTRED}${COLORBOLD}${JUNKCOUNTDOWN}${COLORRESET}${COLORLIGHTRED} deleted pictures ${COLORRESET}"
+
+exit 0
+
+
